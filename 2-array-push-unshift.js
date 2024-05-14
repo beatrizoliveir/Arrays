4ebc33432d8041ca67636b7d83ab7b00d7b1b788
beatrizoliveir <147163128+beatrizoliveir@users.noreply.github.com>
2024-05-14 09:56:15 -0300

    Add files via upload

---
+++ 2-array-push-unshift.js
@@ -0,0 +1,19 @@
+// Definindo um array vazio de frutas 
+var frutas = [];
+
+// Adicionando elementos ao final do array usando o método push
+frutas.push('Maçã');
+frutas.push('Banana');
+frutas.push('Laranja');
+
+ console.log('Array após adicionar elementos ao final:', frutas); // Saída: ['Maçã' , 'Banana', 'Laranja']
+
+// Adicionando elementos ao início do array usando o método unshift
+
+frutas.unshift('Pêssego');
+frutas.unshift('Abacaxi');
+
+  console.log('Array após adicionar elementos ao início:' , frutas); // Saída ['Abacaxi', 'Pêssego', 'Maçã', 'Banana', 'Laranja']
+
+
+
